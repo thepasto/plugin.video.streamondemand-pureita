adfa0cce0eec96cfe9254c4030e552d6472b6abf
orione7 <orione742009@hotmail.it>
2016-04-02 05:55:21 -0400

    Update filmsubitotv.py

--- channels/filmsubitotv.py
+++ channels/filmsubitotv.py
@@ -382,4 +382,4 @@ def findvid(item):
 
 def HomePage(item):
     import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand)")
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita-master)")
