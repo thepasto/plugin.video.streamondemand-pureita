56f9e590e893aed7a1a4525d95138e0ecb579e3e
orione7 <orione742009@hotmail.it>
2016-04-02 05:55:57 -0400

    Update guardarefilm.py

--- channels/guardarefilm.py
+++ channels/guardarefilm.py
@@ -178,7 +178,7 @@ def peliculas(item):
 
 def HomePage(item):
     import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand)")
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita-master)")
 
 def pelis_top100(item):
     logger.info("streamondemand.guardarefilm peliculas")
