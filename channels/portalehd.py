90393a83fd53b288e3736e0dd77b2eb0968d2a2e
orione7 <orione742009@hotmail.it>
2016-04-02 06:03:01 -0400

    Update portalehd.py

--- channels/portalehd.py
+++ channels/portalehd.py
@@ -219,4 +219,4 @@ def anti_cloudflare(url):
 	
 def HomePage(item):
     import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand)")
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita-master)")
