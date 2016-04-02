11c456f8eea75f8effb2fa4dff33edfaebaa9827
orione7 <orione742009@hotmail.it>
2016-04-02 06:02:33 -0400

    Update playcinema.py

--- channels/playcinema.py
+++ channels/playcinema.py
@@ -166,7 +166,7 @@ def peliculas(item):
 
 def HomePage(item):
     import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand)")
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita-master)")
 
 def anti_cloudflare(url):
     # global headers
