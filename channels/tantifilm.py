307e9dab2e0ed9b921ca690220863204ec1de86d
orione7 <orione742009@hotmail.it>
2016-04-02 06:06:46 -0400

    Update tantifilm.py

--- channels/tantifilm.py
+++ channels/tantifilm.py
@@ -207,7 +207,7 @@ def peliculas(item):
 
 def HomePage(item):
     import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand)")
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita-master)")
 
 
 def latest(item):
