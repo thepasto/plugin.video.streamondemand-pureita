06dc2ff2d5bb329aa94fd8c68ce692f5ceb03dbb
orione7 <orione742009@hotmail.it>
2016-04-09 22:36:22 -0400

    Update cinemalibero.py

--- channels/cinemalibero.py
+++ channels/cinemalibero.py
@@ -183,7 +183,7 @@ def peliculas(item):
 
 def HomePage(item):
     import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand)")
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita-master)")
 
 def info(title):
     logger.info("streamondemand.cinemalibero info")
