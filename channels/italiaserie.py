7a5f5ab67fb0a17e710aabf0f93c5ae51bc9cad2
orione7 <orione742009@hotmail.it>
2016-04-09 22:46:26 -0400

    Update italiaserie.py

--- channels/italiaserie.py
+++ channels/italiaserie.py
@@ -126,7 +126,7 @@ def peliculas(item):
 
 def HomePage(item):
     import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand)")
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita-master)")
 
 def peliculas2(item):
     logger.info("streamondemand.italiaserie peliculas")
