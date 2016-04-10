abc76d3a48672b64283438d30d1825409477805c
orione7 <orione742009@hotmail.it>
2016-04-09 22:47:54 -0400

    Update piratestreaming.py

--- channels/piratestreaming.py
+++ channels/piratestreaming.py
@@ -139,7 +139,7 @@ def peliculas(item):
 
 def HomePage(item):
     import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand)")
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita-master)")
 
 def categorias(item):
     itemlist = []
