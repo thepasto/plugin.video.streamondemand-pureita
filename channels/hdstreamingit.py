2d10cceca70194861d6d85998ba4c4a1a9529282
orione7 <orione742009@hotmail.it>
2016-04-09 22:44:35 -0400

    Update hdstreamingit.py

--- channels/hdstreamingit.py
+++ channels/hdstreamingit.py
@@ -239,7 +239,7 @@ def pelis3d(item):
 
 def HomePage(item):
     import xbmc
-    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand)")
+    xbmc.executebuiltin("ReplaceWindow(10024,plugin://plugin.video.streamondemand-pureita-master)")
 
 def episodios(item):
     logger.info("streamondemand.hdstreamingit episodios")
