77772e4e581298430f0dbde24b6ff5f1db893d1d
orione7 <orione742009@hotmail.it>
2016-05-29 05:20:06 -0400

    Update PureIta

--- channels/soloserie.py
+++ channels/soloserie.py
@@ -2,7 +2,7 @@
 # ------------------------------------------------------------
 # streamondemand-pureita-master.- XBMC Plugin
 # Canale per solo-streaming.com
-# http://blog.tvalacarta.info/plugin-xbmc/streamondemand-pureita-master.
+# http://blog.tvalacarta.info/plugin-xbmc/streamondemand-pureita.
 # ------------------------------------------------------------
 import urlparse,urllib2,urllib, json
 import re
