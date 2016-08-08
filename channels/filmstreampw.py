0e7858d5b860c958f947b37a461cb25a6fcf427c
orione7 <orione742009@hotmail.it>
2016-08-08 02:31:23 -0400

    Update PureITA 5.3.8

--- channels/filmstreampw.py
+++ channels/filmstreampw.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------
 # streamondemand.- XBMC Plugin
-# Canal para piratestreaming
+# Canale per piratestreaming
 # http://blog.tvalacarta.info/plugin-xbmc/streamondemand.
 # ------------------------------------------------------------
 import re
