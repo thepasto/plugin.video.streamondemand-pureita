4c65f7fe7ba08c6905da3a5d9332ff99782191ba
orione7 <orione742009@hotmail.it>
2016-11-15 07:26:23 +0100

    Update channelselector.py

--- channelselector.py
+++ channelselector.py
@@ -310,7 +310,7 @@ def channels_list():
     itemlist.append( Item( title="[COLOR azure]LiberoITA[/COLOR]"       , channel="liberoita"           , language="IT"    , category="F,S,A"   , type="generic"))
     itemlist.append( Item(title="[COLOR azure]LeserieTV[/COLOR]"    , channel="leserietv"       , language="IT"    , category="B,S"     , type="generic" ))   
     itemlist.append( Item( title="[COLOR azure]Megafiletube[/COLOR]"        , channel="megafiletube"       , language="IT"    , category="F,T"       , type="generic"	))
-    itemlist.append( Item( title="[COLOR azure]Misterstreaming[/COLOR]"        , channel="misterstreaming"       , language="IT"    , category="F,S"       , type="generic"	))
+    #itemlist.append( Item( title="[COLOR azure]Misterstreaming[/COLOR]"        , channel="misterstreaming"       , language="IT"    , category="F,S"       , type="generic"	))
     itemlist.append( Item(title="[COLOR azure]MondoLunatico[/COLOR]"    , channel="mondolunatico"       , language="IT"    , category="F"     , type="generic" ))
     #itemlist.append( Item( title="[COLOR azure]Multiplayer[/COLOR]"        , channel="multiplayer"       , language="IT"    , category="D"       , type="generic"))
     #itemlist.append( Item( title="[COLOR azure]Liberostreaming[/COLOR]" , channel="liberostreaming" , language="IT" , category="F,S,A" , type="generic"))
