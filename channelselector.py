2994faca9cac1ad53de7d3737d2ddf4f06ee9868
orione7 <orione742009@hotmail.it>
2016-12-09 19:06:47 +0100

    Update "filmhdstreaming"

--- channelselector.py
+++ channelselector.py
@@ -281,7 +281,7 @@ def channels_list():
     #itemlist.append( Item( title="[COLOR azure]Fastvideo.tv[/COLOR]"        , channel="filmitaliatv"       , language="IT"    , category="F"       , type="generic"))
     #itemlist.append( Item( title="[COLOR azure]FilmGratis.cc[/COLOR]"       , channel="filmgratiscc"           , language="IT"    , category="F"   , type="generic"))
     itemlist.append( Item( title="[COLOR azure]Filmbelli.net[/COLOR]"          , channel="filmbelli"           , language="IT"    , category="F"   , type="generic"))
-    #itemlist.append( Item( title="[COLOR azure]Filmhdstreaming[/COLOR]"          , channel="filmhdstreaming"           , language="IT"    , category="F"   , type="generic"))
+    itemlist.append( Item( title="[COLOR azure]Filmhdstreaming[/COLOR]"          , channel="filmhdstreaming"           , language="IT"    , category="F"   , type="generic"))
     itemlist.append( Item( title="[COLOR azure]Filmissimi[/COLOR]"          , channel="filmissimi"           , language="IT"    , category="F"   , type="generic"))
     itemlist.append( Item( title="[COLOR azure]FilmStream.org[/COLOR]"          , channel="filmstream"           , language="IT"    , category="F,S"   , type="generic"))
     itemlist.append( Item( title="[COLOR azure]FilmStream.to[/COLOR]"       , channel="filmstreampw"           , language="IT"    , category="F,S"   , type="generic"))
