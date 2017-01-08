b3dd74410fefb57731ee8a087d45abd246442294
orione7 <italstream2@gmail.com>
2017-01-08 22:16:02 +0100

    Update mondolunatico.py

--- channels/mondolunatico.py
+++ channels/mondolunatico.py
@@ -22,6 +22,9 @@ __type__ = "generic"
 __title__ = "Mondo Lunatico"
 __language__ = "IT"
 
+
+DEBUG = config.get_setting("debug")
+
 host = "http://mondolunatico.org"
 
 captcha_url = '%s/pass/CaptchaSecurityImages.php?width=100&height=40&characters=5' % host
@@ -33,7 +36,7 @@ headers = [
     ['Accept-Encoding', 'gzip, deflate']
 ]
 
-DEBUG = config.get_setting("debug")
+PERPAGE = 25
 
 
 def isGeneric():
