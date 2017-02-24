bb4eb0b2a6895e72c6793c11252bf5f069d8e300
orione7 <italstream2@gmail.com>
2017-02-24 03:29:03 +0100

    Update cineblog01.py

--- channels/cineblog01.py
+++ channels/cineblog01.py
@@ -20,7 +20,7 @@ __type__ = "generic"
 __title__ = "CineBlog 01"
 __language__ = "IT"
 
-sito = "http://www.cb01.pw"
+sito = "http://www.cb01.uno"
 
 headers = [
     ['User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0'],
