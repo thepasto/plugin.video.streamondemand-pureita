b290f5a4dbcfd8d5071fd79cc7f934438f2229ea
orione7 <italstream2@gmail.com>
2017-03-15 22:09:09 +0100

    Update movshare.py

--- servers/movshare.py
+++ servers/movshare.py
@@ -19,7 +19,7 @@ def test_video_exists( page_url ):
     data = scrapertools.cache_page(page_url)
     
     if "This file no longer exists on our servers" in data:
-        return False,"El fichero ha sido borrado de movshare"
+        return False, "File rimosso"
 
     return True,""
 
@@ -27,20 +27,20 @@ def test_video_exists( page_url ):
 def get_video_url( page_url , premium = False , user="" , password="" , video_password="" ):
     logger.info("[movshare.py] get_video_url(page_url='%s')" % page_url)
 
-    videoid = scrapertools.get_match(page_url,"http://www.movshare.net/video/([a-z0-9]+)")
+    videoid = scrapertools.get_match(page_url,"http://www.movshare.ag/video/([a-z0-9]+)")
     video_urls = []
 
-    # Descarga la p·gina
+    # Descarga la p√°gina
     headers = []
     headers.append( ['User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'] )
     html = scrapertools.cache_page(page_url , headers = headers)
     
-    # La vuelve a descargar, como si hubieras hecho click en el botÛn
+    # La vuelve a descargar, como si hubieras hecho click en el bot√≥n
     #html = scrapertools.cache_page(page_url , headers = headers)
     filekey = scrapertools.find_single_match(html,'flashvars.filekey="([^"]+)"')
         
     #get stream url from api
-    api = 'http://www.movshare.net/api/player.api.php?key=%s&file=%s' % (filekey, videoid)
+    api = 'http://www.movshare.ag/api/player.api.php?key=%s&file=%s' % (filekey, videoid)
     headers.append( ['Referer',page_url] )
 
     html = scrapertools.cache_page(api,headers=headers)
@@ -55,19 +55,19 @@ def get_video_url( page_url , premium = False , user="" , password="" , video_pa
 
     return video_urls
 
-# Encuentra vÌdeos del servidor en el texto pasado
+# Encuentra v√≠deos del servidor en el texto pasado
 def find_videos(data):
     encontrados = set()
     devuelve = []
 
     #http://www.movshare.net/video/deg0ofnrnm8nq
-    patronvideos  = 'movshare.net/video/([a-z0-9]+)'
+    patronvideos  = 'movshare.ag/video/([a-z0-9]+)'
     logger.info("[movshare.py] find_videos #"+patronvideos+"#")
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
 
     for match in matches:
         titulo = "[movshare]"
-        url = "http://www.movshare.net/video/"+match
+        url = "http://www.movshare.ag/video/"+match
 
         if url not in encontrados:
             logger.info("  url="+url)
@@ -77,13 +77,13 @@ def find_videos(data):
             logger.info("  url duplicada="+url)
 
     #
-    patronvideos  = "movshare.net/embed/([a-z0-9]+)"
+    patronvideos  = "movshare.ag/embed/([a-z0-9]+)"
     logger.info("[movshare.py] find_videos #"+patronvideos+"#")
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
 
     for match in matches:
         titulo = "[movshare]"
-        url = "http://www.movshare.net/video/"+match
+        url = "http://www.movshare.ag/video/"+match
 
         if url not in encontrados:
             logger.info("  url="+url)
@@ -93,13 +93,13 @@ def find_videos(data):
             logger.info("  url duplicada="+url)
 
     #http://embed.movshare.net/embed.php?v=xepscujccuor7&width=1000&height=450
-    patronvideos  = "movshare.net/embed.php\?v\=([a-z0-9]+)"
+    patronvideos  = "movshare.ag/embed.php\?v\=([a-z0-9]+)"
     logger.info("[movshare.py] find_videos #"+patronvideos+"#")
     matches = re.compile(patronvideos,re.DOTALL).findall(data)
 
     for match in matches:
         titulo = "[movshare]"
-        url = "http://www.movshare.net/video/"+match
+        url = "http://www.movshare.ag/video/"+match
 
         if url not in encontrados:
             logger.info("  url="+url)
