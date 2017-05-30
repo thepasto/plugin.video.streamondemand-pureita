dc86f0b463afe1e75c39a3f78df83ab5341f583f
orione7 <italstream2@gmail.com>
2017-05-30 23:40:51 +0200

    Update altadefinizione01.py

--- channels/altadefinizione01.py
+++ channels/altadefinizione01.py
@@ -118,6 +118,7 @@ def peliculas(item):
             Item(channel=__channel__,
                  action="HomePage",
                  title="[COLOR yellow]Torna Home[/COLOR]",
+                 thumbnail="https://raw.githubusercontent.com/orione7/Pelis_images/master/channels_icon_pureita/return_home_P.png",
                  folder=True)),
         itemlist.append(
             Item(channel=__channel__,
