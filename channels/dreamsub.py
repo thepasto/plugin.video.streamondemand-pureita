0ea73e0da85edff93b3413347e0e61c70bbcffbd
orione7 <italstream2@gmail.com>
2017-06-06 20:14:16 +0200

    Update dreamsub.py

--- channels/dreamsub.py
+++ channels/dreamsub.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------
 # streamondemand-pureita.- XBMC Plugin
-# Canale per dreamsub
+# Canale  dreamsub
 # http://www.mimediacenter.info/foro/viewtopic.php?f=36&t=7808
 # ------------------------------------------------------------
 import re
@@ -168,6 +168,9 @@ def ultimiep(item):
     matches = re.compile(patron, re.DOTALL).findall(bloque)
 
     for scrapedurl, scrapedtitle in matches:
+        if 'anime' in item.extra:
+            ep = scrapertools.find_single_match(scrapedtitle, r'\d+$').zfill(2)
+            scrapedtitle = re.sub(r'\d+$', ep, scrapedtitle)
         scrapedurl = host + scrapedurl
         scrapedplot = ""
         scrapedthumbnail = ""
@@ -176,7 +179,7 @@ def ultimiep(item):
         itemlist.append(infoSod(
             Item(channel=__channel__,
                  action="findvideos",
-                 fulltitle=(re.sub(r'\d+$', '', scrapedtitle) if 'anime' in item.extra else re.sub(r'\d+x\d+$', '', scrapedtitle)).strip(),
+                 fulltitle=(re.sub(r'\d*-?\d+$', '', scrapedtitle) if 'anime' in item.extra else re.sub(r'\d+x\d+$', '', scrapedtitle)).strip(),
                  show=scrapedtitle,
                  title=scrapedtitle,
                  url=scrapedurl,
