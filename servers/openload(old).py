3bcea79e56ef918bd33de3089dd46bf7d683d0ec
orione7 <streamondemandpureita@gmail.com>
2017-09-24 18:38:41 -0400

    Update Servers

--- servers/openload(old).py
+++ servers/openload(old).py
@@ -27,7 +27,7 @@ def test_video_exists(page_url):
     if 'We’re Sorry!' in data:
         data = httptools.downloadpage(page_url.replace("/embed/", "/f/"), headers=header, cookies=False).data
         if 'We’re Sorry!' in data:
-            return False, "[Openload] Il file non esiste o e stato cancellato"
+            return False, "[Openload] File eliminato" 
 
     return True, ""
 
@@ -42,7 +42,7 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
         header = {'Referer': referer}
     data = httptools.downloadpage(page_url, headers=header, cookies=False).data
     subtitle = scrapertools.find_single_match(data, '<track kind="captions" src="([^"]+)" srclang="es"')
-    # Header para la descarga
+    #Header para la descarga
     header_down = "|User-Agent=" + headers['User-Agent']
 
     try:
@@ -58,11 +58,10 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
 
         var_r = scrapertools.find_single_match(text_decode, "window\.[A-z]+\s*=\s*['\"]([^'\"]+)['\"]")
         var_encodes = scrapertools.find_multiple_matches(data, 'id="%s[^"]*">([^<]+)<' % var_r)
-        numeros = scrapertools.find_single_match(data, '_[A-f0-9]+x[A-f0-9]+\s*(?:=|\^)\s*([0-9]{4,}|0x[A-f0-9]{4,})')
+        numeros = scrapertools.find_multiple_matches(data, '_[A-f0-9]+x[A-f0-9]+\s*(?:=|\^)\s*([0-9]{4,}|0x[A-f0-9]{4,})')
+        numeros8 = scrapertools.find_multiple_matches(data, "parseInt\('([^']+)',8\)")
         op1, op2 = scrapertools.find_single_match(data, '\(0x(\d),0x(\d)\);')
-        idparse, hexparse = scrapertools.find_multiple_matches(data, "parseInt\('([0-9]+)'")
-        # numeros = [numeros, str(int(hexparse, 8))]
-        rangos, rangos2 = scrapertools.find_single_match(data, "\)-([0-9]+).0x4\)/\(([0-9]+)")
+
         videourl = ""
         for encode in var_encodes:
             text_decode = ""
@@ -71,7 +70,7 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
                 rango1 = encode[:mult]
                 decode1 = []
                 for i in range(0, len(rango1), 8):
-                    decode1.append(int(rango1[i:i + 8], 16))
+                    decode1.append(int(rango1[i:i+8], 16))
                 rango1 = encode[mult:]
                 j = 0
                 i = 0
@@ -83,7 +82,7 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
                     while True:
                         if (i + 1) >= len(rango1):
                             index1 = 143
-                        value3 = int(rango1[i:i + 2], 16)
+                        value3 = int(rango1[i:i+2], 16)
                         i += 2
                         data = value3 & 63
                         value2 += data << value1
@@ -91,13 +90,14 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
                         if value3 < index1:
                             break
 
-                    # value4 = value2 ^ decode1[j % (mult / 8)] ^ int(idparse,8)
-                    # for n in numeros:
-                    #     if not n.isdigit():
-                    #         n = int(n, 16)
-                    #     value4 ^= int(n)
-                    value4 = value2 ^ decode1[(j % 9)] ^ (int(idparse, 8) - int(rangos) + 4) / (int(rangos2) - 8) ^ int(hexparse, 8)
-                    value5 = index1 * 2 + 127
+                    value4 = value2 ^ decode1[j % (mult/8)]
+                    for n in numeros8:
+                        value4 ^= int(n, 8)
+                    for n in numeros:
+                        if not n.isdigit():
+                            n = int(n, 16)
+                        value4 ^= int(n)
+                    value5 = index1 * 2 + 127 
                     for h in range(4):
                         valorfinal = (value4 >> 8 * h) & (value5)
                         valorfinal = chr(valorfinal - 1)
@@ -141,27 +141,6 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     return video_urls
 
 
-def get_link_api(page_url):
-    from core import jsontools
-    file_id = scrapertools.find_single_match(page_url, '(?:embed|f)/([0-9a-zA-Z-_]+)')
-    login = "97b2326d7db81f0f"
-    key = "AQFO3QJQ"
-    data = httptools.downloadpage(
-        "https://api.openload.co/1/file/dlticket?file=%s&login=%s&key=%s" % (file_id, login, key)).data
-    data = jsontools.load(data)
-
-    if data["status"] == 200:
-        ticket = data["result"]["ticket"]
-        data = httptools.downloadpage("https://api.openload.co/1/file/dl?file=%s&ticket=%s" % (file_id, ticket)).data
-        data = jsontools.load(data)
-        extension = data["result"]["content_type"]
-        videourl = data['result']['url']
-        videourl = videourl.replace("https", "http")
-        return videourl, extension
-
-    return "", ""
-
-
 # Encuentra vídeos del servidor en el texto pasado
 def find_videos(text):
     encontrados = set()
@@ -187,4 +166,21 @@ def find_videos(text):
     return devuelve
 
 
+def get_link_api(page_url):
+    from core import jsontools
+    file_id = scrapertools.find_single_match(page_url, '(?:embed|f)/([0-9a-zA-Z-_]+)')
+    login = "97b2326d7db81f0f"
+    key = "AQFO3QJQ"
+    data = httptools.downloadpage("https://api.openload.co/1/file/dlticket?file=%s&login=%s&key=%s" % (file_id, login, key)).data
+    data = jsontools.load_json(data)
 
+    if data["status"] == 200:
+        ticket = data["result"]["ticket"]
+        data = httptools.downloadpage("https://api.openload.co/1/file/dl?file=%s&ticket=%s" % (file_id, ticket)).data
+        data = jsontools.load_json(data)
+        extension = data["result"]["content_type"]
+        videourl = data['result']['url']
+        videourl = videourl.replace("https", "http")
+        return videourl, extension
+
+    return "", ""
