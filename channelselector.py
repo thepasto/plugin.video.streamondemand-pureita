957dc8f5cb2f4bbd730362139bb57cf1771163db
orione7 <streamondemandpureita@gmail.com>
2017-12-12 03:05:54 -0500

    Update New Channel

--- channelselector.py
+++ channelselector.py
@@ -330,7 +330,7 @@ def channels_list():
     #itemlist.append( Item( title="[COLOR azure]LiberoITA[/COLOR]"       , channel="liberoita"           , language="IT"    , category="F,S,A"   , type="generic"))
     itemlist.append( Item(title="[COLOR azure]LeserieTV[/COLOR]"    , channel="leserietv"       , language="IT"    , category="B,S"     , type="generic" ))   
     itemlist.append( Item( title="[COLOR azure]Majintoon[/COLOR]"      , channel="majintoon"           , language="IT"    , category="A,S"   , type="generic"))
-    #itemlist.append( Item( title="[COLOR azure]Megafiletube[/COLOR]"        , channel="megafiletube"       , language="IT"    , category="T"       , type="generic"	))
+    itemlist.append( Item( title="[COLOR azure]Megavideo One[/COLOR]"        , channel="megavideo_one"       , language="IT"    , category="F"       , type="generic"	))
     #itemlist.append( Item( title="[COLOR azure]Misterstreaming[/COLOR]"        , channel="misterstreaming"       , language="IT"    , category="F,S"       , type="generic"	))
     itemlist.append( Item(title="[COLOR azure]MondoLunatico[/COLOR]"    , channel="mondolunatico"       , language="IT"    , category="F"     , type="generic" ))
     itemlist.append( Item(title="[COLOR azure]MondoLunaticoHD[/COLOR]"    , channel="mondolunatico_hd"       , language="IT"    , category="F"     , type="generic" ))
