272a0e1f2af254bd1e6007e057f7d90f04e7c468
orione7 <streamondemandpureita@gmail.com>
2018-01-23 03:49:42 -0500

    Update and Fix

--- channels/netlover.py
+++ channels/netlover.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------
 # StreamOnDemand-PureITA / XBMC Plugin
-# Canale Netlover (search engine)"
+# Canale .NetLover (search engine)"
 # http://www.mimediacenter.info/foro/viewtopic.php?f=36&t=7808
 # ------------------------------------------------------------
 
@@ -16,13 +16,7 @@ from core.item import Item
 from core.tmdb import infoSod
 
 __channel__ = "netlover"
-__category__ = "S"
-__type__ = "generic"
-__title__ = "netlover search (IT)"
-__language__ = "IT"
-
 DEBUG = config.get_setting("debug")
-
 host = "https://www.netflixlovers.it"
 
 TIMEOUT_TOTAL = 45
@@ -33,6 +27,11 @@ def isGeneric():
 def mainlist(item):
     logger.info("streamondemand-pureita.netlover mainlist")
     itemlist = [Item(channel=__channel__,
+                     title="[COLOR azure]Elenco Completo - [COLOR red].NET Lover[/COLOR]",
+                     action="originals",
+                     url="%s/netflix-originals-elenco-completo/" % host,
+                     thumbnail="https://raw.githubusercontent.com/orione7/Pelis_images/master/channels_icon_pureita/popcorn_movie_P.png"),
+	            Item(channel=__channel__,
                      title="[COLOR azure]TOP 7 Days - [COLOR red].NET Lover[/COLOR]",
                      action="film",
                      url="%s/classifiche/ultimi-7-giorni" % host,
@@ -60,6 +59,8 @@ def mainlist(item):
 
     return itemlist
 
+# ==============================================================================================================================================================================	
+
 def serietv(item):
     logger.info("streamondemand-pureita.netlover serietv")
     itemlist = []
@@ -68,12 +69,14 @@ def serietv(item):
     data = scrapertools.cache_page(item.url)
 
     # Extrae las entradas (carpetas)
-    patron = '<span class="thumb-info-title">\s*<span class="thumb-info-caption-text">([^<]+)</span>'
+    patron = '<img src="([^"]+)"[^>]+>\s*[^>]+>\s*<span class="thumb-info-title">\s*<span class="thumb-info-caption-text">([^<]+)</span>.*?'
+    patron += '<p>([^"]+)</p>'
     matches = re.compile(patron, re.DOTALL).findall(data)
 
-    for scrapedtitle in matches:
+    for scrapedthumbnail, scrapedtitle, scrapedplot in matches:
+        scrapedtitle = scrapedtitle.replace(":", " -")
+        scrapedtitle = scrapedtitle.replace("&#39;", " ")
         scrapedurl = ""
-        scrapedthumbnail = ""
         scrapedtv = ".NET Lover"
         scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
         scrapedtitle = scrapedtitle.split("(")[0]
@@ -85,11 +88,12 @@ def serietv(item):
                  title=scrapedtitle + "[COLOR red]   " + scrapedtv + "[/COLOR]",
                  fulltitle=scrapedtitle,
                  url=scrapedurl,
+                 plot=scrapedplot,
                  thumbnail=scrapedthumbnail,
                  folder=True), tipo="tv"))
 
     # Extrae el paginador
-    patronvideos = '<a href="([^"]+)" class="btn btn-borders btn-primary mr-xs mb-sm center">Mostra'
+    patronvideos = '<a href="([^"]+)" class="btn btn-borders btn-primary mr-xs mb-sm center">Mostra.*?</a>'
     matches = re.compile(patronvideos, re.DOTALL).findall(data)
 
     if len(matches) > 0:
@@ -104,7 +108,9 @@ def serietv(item):
 
     return itemlist
 
-def film(item):
+# ==============================================================================================================================================================================
+
+def originals(item):
     logger.info("streamondemand-pureita.netlover film")
     itemlist = []
 
@@ -112,12 +118,15 @@ def film(item):
     data = scrapertools.cache_page(item.url)
 
     # Extrae las entradas (carpetas)
-    patron = '<span class="thumb-info-title">\s*<span class="thumb-info-caption-text">([^<]+)</span>'
+    patron = '<img src="([^"]+)"[^>]+alt="([^<]+)"\s*title[^>]+>\s*[^>]+>.*?'
+    patron += '<span class="thumb-info-caption-text">([^"]+)</span>'
     matches = re.compile(patron, re.DOTALL).findall(data)
 
-    for scrapedtitle in matches:
+    for scrapedthumbnail, scrapedtitle, scrapedplot in matches:
+        scrapedtitle = scrapedtitle.replace(":", " -")
+        scrapedtitle = scrapedtitle.replace("&#39;", " ")
+        scrapedtitle = scrapedtitle.replace("Locandina di", "")
         scrapedurl = ""
-        scrapedthumbnail = ""
         scrapedtv = ".NET Lover"
         scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
         scrapedtitle = scrapedtitle.split("(")[0]
@@ -129,11 +138,61 @@ def film(item):
                  title=scrapedtitle + "[COLOR red]   " + scrapedtv + "[/COLOR]",
                  fulltitle=scrapedtitle,
                  url=scrapedurl,
+                 plot=scrapedplot,
+                 thumbnail=scrapedthumbnail,
+                 folder=True), tipo="movie"))
+
+
+    patronvideos = '<a href="([^"]+)" class="btn btn-borders btn-primary mr-xs mb-sm center">Mostra.*?</a>'
+    matches = re.compile(patronvideos, re.DOTALL).findall(data)
+
+    if len(matches) > 0:
+        scrapedurl = urlparse.urljoin(item.url, matches[0])
+        itemlist.append(
+            Item(channel=__channel__,
+                 action="originals",
+                 title="[COLOR orange]Successivo >>[/COLOR]",
+                 url=scrapedurl,
+                 thumbnail="https://raw.githubusercontent.com/orione7/Pelis_images/master/channels_icon_pureita/next_1.png",
+                 folder=True))
+
+    return itemlist
+
+# ==============================================================================================================================================================================
+	
+def film(item):
+    logger.info("streamondemand-pureita.netlover film")
+    itemlist = []
+
+    # Descarga la pagina
+    data = scrapertools.cache_page(item.url)
+
+    # Extrae las entradas (carpetas)
+    patron = '<img src="([^"]+)"[^>]+>\s*[^>]+>\s*<span class="thumb-info-title">\s*<span class="thumb-info-caption-text">([^<]+)</span>.*?'
+    patron += '<p>([^"]+)</p>'
+    matches = re.compile(patron, re.DOTALL).findall(data)
+
+    for scrapedthumbnail, scrapedtitle, scrapedplot in matches:
+        scrapedtitle = scrapedtitle.replace(":", " -")
+        scrapedtitle = scrapedtitle.replace("&#39;", " ")
+        scrapedurl = ""
+        scrapedtv = ".NET Lover"
+        scrapedtitle = scrapertools.decodeHtmlentities(scrapedtitle)
+        scrapedtitle = scrapedtitle.split("(")[0]
+
+        itemlist.append(infoSod(
+            Item(channel=__channel__,
+                 action="do_search",
+                 extra=urllib.quote_plus(scrapedtitle),
+                 title=scrapedtitle + "[COLOR red]   " + scrapedtv + "[/COLOR]",
+                 fulltitle=scrapedtitle,
+                 url=scrapedurl,
+                 plot=scrapedplot,
                  thumbnail=scrapedthumbnail,
                  folder=True), tipo="movie"))
 
     # Extrae el paginador
-    patronvideos = '<a href="([^"]+)" class="btn btn-borders btn-primary mr-xs mb-sm center">Mostra'
+    patronvideos = '<a href="([^"]+)" class="btn btn-borders btn-primary mr-xs mb-sm center">Mostra.*?</a>'
     matches = re.compile(patronvideos, re.DOTALL).findall(data)
 
     if len(matches) > 0:
@@ -148,7 +207,7 @@ def film(item):
 
     return itemlist
 
-# Esta es la función que realmente realiza la búsqueda
+# ==============================================================================================================================================================================
 
 def do_search(item):
     from channels import buscador
