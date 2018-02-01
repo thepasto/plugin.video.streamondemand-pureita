7b47e924baec51ba714934fefd93c5a22df3dde6
orione7 <streamondemandpureita@gmail.com>
2018-02-01 05:06:32 -0500

    Update and Fix

--- channelselector.py
+++ channelselector.py
@@ -261,7 +261,7 @@ def channels_list():
     itemlist.append( Item( title="[COLOR azure]Animestream.it[/COLOR]"   , channel="animestream"           , language="IT"    , category="A"   , type="generic"))
     itemlist.append( Item( title="[COLOR azure]AnimeTubeIta.com[/COLOR]"   , channel="animetubeita"           , language="IT"    , category="A"   , type="generic"))
     itemlist.append( Item( title="[COLOR azure]Animevision[/COLOR]"   , channel="animevision"           , language="IT"    , category="A"   , type="generic"))
-    itemlist.append( Item( title="[COLOR azure]BleachAnimeManga[/COLOR]"   , channel="bleachanimemanga"           , language="IT"    , category="A"   , type="generic"))
+    #itemlist.append( Item( title="[COLOR azure]BleachAnimeManga[/COLOR]"   , channel="bleachanimemanga"           , language="IT"    , category="A"   , type="generic"))
     itemlist.append( Item( title="[COLOR azure]AnimeSenzaLimiti[/COLOR]"      , channel="animesenzalimiti"           , language="IT"    , category="A"   , type="generic"))
     #itemlist.append( Item( title="[COLOR azure]Asian Sub-Ita[/COLOR]"      , channel="asiansubita"           , language="IT"    , category="F,S,VOS"   , type="generic"))
     #itemlist.append( Item( title="[COLOR azure]BreakingBadITA Streaming[/COLOR]"      , channel="breakingbadita"           , language="IT"    , category="S"   , type="generic"))
