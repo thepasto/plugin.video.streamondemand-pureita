fc15398e399f85c976d91958962dc2a31205d928
orione7 <streamondemandpureita@gmail.com>
2018-03-23 15:44:52 -0400

    Update and Fix

--- channels/guardarefilm.py
+++ channels/guardarefilm.py
@@ -21,7 +21,7 @@ __type__ = "generic"
 __title__ = "guardarefilm (IT)"
 __language__ = "IT"
 
-host = "https://www.guardarefilm.zone/"
+host = "https://www.guardarefilm.life/"
 
 headers = [
     ['User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:38.0) Gecko/20100101 Firefox/38.0'],
