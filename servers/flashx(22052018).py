327cea9312acfa6c4ee7d659f2ec4aefbe43ae48
orione7 <streamondemandpureita@gmail.com>
2018-05-22 19:56:26 -0400

    Update and Fix

--- servers/flashx(22052018).py
+++ servers/flashx(22052018).py
@@ -23,6 +23,12 @@ from lib import jsunpack
 def test_video_exists(page_url):
     logger.info("(page_url='%s')" % page_url)
 
+    data = httptools.downloadpage(page_url, cookies=False).data
+    if 'file was deleted' in data:
+        return False, "[FlashX] File non presente"
+    elif 'Video is processing now' in data:
+        return False, "[FlashX] File in lavorazione"
+
     return True, ""
 
 
@@ -31,11 +37,11 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     pfxfx = ""
     data = httptools.downloadpage(page_url, cookies=False).data
     data = data.replace("\n","")
-    cgi_counter = scrapertools.find_single_match(data, """(?is)src=.(https://www.flashx.cc/counter.cgi.*?[^(?:'|")]+)""")
+    cgi_counter = scrapertools.find_single_match(data, """(?is)src=.(https://www.flashx.bz/counter.cgi.*?[^(?:'|")]+)""")
     cgi_counter = cgi_counter.replace("%0A","").replace("%22","")
-    playnow = scrapertools.find_single_match(data, 'https://www.flashx.cc/dl[^"]+')
+    playnow = scrapertools.find_single_match(data, 'https://www.flashx.bz/dl[^"]+')
     # Para obtener el f y el fxfx
-    js_fxfx = "https://www." + scrapertools.find_single_match(data.replace("//","/"), """(?is)(flashx.cc/js\w+/c\w+.*?[^(?:'|")]+)""")
+    js_fxfx = "https://www." + scrapertools.find_single_match(data.replace("//","/"), """(?is)(flashx.bz/js\w+/c\w+.*?[^(?:'|")]+)""")
     data_fxfx = httptools.downloadpage(js_fxfx).data
     mfxfx = scrapertools.find_single_match(data_fxfx, 'get.*?({.*?})').replace("'","").replace(" ","")
     matches = scrapertools.find_multiple_matches(mfxfx, '(\w+):(\w+)')
@@ -45,7 +51,7 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     logger.info("mfxfxfx2= %s" %pfxfx)
     if pfxfx == "":
         pfxfx = "ss=yes&f=fail&fxfx=6"
-    coding_url = 'https://www.flashx.cc/flashx.php?%s' %pfxfx
+    coding_url = 'https://www.flashx.bz/flashx.php?%s' %pfxfx
     # {f: 'y', fxfx: '6'}
     bloque = scrapertools.find_single_match(data, '(?s)Form method="POST" action(.*?)span')
     flashx_id = scrapertools.find_single_match(bloque, 'name="id" value="([^"]+)"')
