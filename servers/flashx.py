d6ac22700f4c2ae87cf19bf3a07c4da2087f4e84
orione7 <streamondemandpureita@gmail.com>
2018-07-30 15:55:32 -0400

    Update and Fix

--- servers/flashx.py
+++ servers/flashx.py
@@ -33,7 +33,7 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     data = data.replace("\n","")
     cgi_counter = scrapertools.find_single_match(data, """(?is)src=.(https://www.flashx.cc/counter.cgi.*?[^(?:'|")]+)""")
     cgi_counter = cgi_counter.replace("%0A","").replace("%22","")
-    playnow = scrapertools.find_single_match(data, 'https://www.flashx.cc/dl[^"]+')
+    playnow = scrapertools.find_single_match(data, 'https://www.flashx.co/dl[^"]+')
     # Para obtener el f y el fxfx
     js_fxfx = "https://www." + scrapertools.find_single_match(data.replace("//","/"), """(?is)(flashx.cc/js\w+/c\w+.*?[^(?:'|")]+)""")
     data_fxfx = httptools.downloadpage(js_fxfx).data
@@ -45,7 +45,7 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     logger.info("mfxfxfx2= %s" %pfxfx)
     if pfxfx == "":
         pfxfx = "ss=yes&f=fail&fxfx=6"
-    coding_url = 'https://www.flashx.cc/flashx.php?%s' %pfxfx
+    coding_url = 'https://www.flashx.co/flashx.php?%s' %pfxfx
     # {f: 'y', fxfx: '6'}
     bloque = scrapertools.find_single_match(data, '(?s)Form method="POST" action(.*?)span')
     flashx_id = scrapertools.find_single_match(bloque, 'name="id" value="([^"]+)"')
