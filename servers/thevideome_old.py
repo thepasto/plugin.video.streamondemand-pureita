118d044fb79eaea1cc2097ca7363c200bb9772b9
orione7 <streamondemandpureita@gmail.com>
2018-08-12 06:23:05 -0400

    Update and Fix

--- servers/thevideome_old.py
+++ servers/thevideome_old.py
@@ -1,46 +1,49 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------
-# streamondemand-pureita - XBMC Plugin
-# Conector para thevideo
-# http://www.mimediacenter.info/foro/viewtopic.php?f=36&t=7808
-# Alfa - Alfa-PureITA
+# streamondemand - XBMC Plugin
+# Conector para thevideo.me
+# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
 # ------------------------------------------------------------
 
 import re
-import time
-import urllib
-import xbmc
 
 from core import httptools
-from lib import jsunpack
-from core import config
 from core import logger
 from core import scrapertools
+
 
 def test_video_exists(page_url):
     logger.info("(page_url='%s')" % page_url)
 
     data = httptools.downloadpage(page_url).data
     if "File was deleted" in data or "Page Cannot Be Found" in data:
-        return False, "[thevideo.me] File Cancellato"
+        return False, "[thevideo.me] File eliminato"
 
     return True, ""
 
 
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
     logger.info("url=" + page_url)
-    video_urls = []
-    post= {}
-    post = urllib.urlencode(post)
     if not "embed" in page_url:
-        page_url = page_url.replace("https://thevideo.me/", "https://thevideo.me/embed-") + ".html"
-    url = httptools.downloadpage(page_url, follow_redirects=False, only_headers=True).headers.get("location", "")
-    data = httptools.downloadpage("https://vev.io/api/serve/video/" + scrapertools.find_single_match(url, "embed/([A-z0-9]+)"), post=post).data
-    bloque = scrapertools.find_single_match(data, 'qualities":\{(.*?)\}')
-    matches = scrapertools.find_multiple_matches(bloque, '"([^"]+)":"([^"]+)')
-    for res, media_url in matches:
+        page_url = page_url.replace("http://thevideo.me/", "http://thevideo.me/embed-") + ".html"
+
+    data = httptools.downloadpage(page_url).data
+
+    var = scrapertools.find_single_match(data, 'vsign.player.*?\+ (\w+)')
+    mpri_Key = scrapertools.find_single_match(data, "%s='([^']+)'" %var)
+    data_vt = httptools.downloadpage("https://thevideo.me/vsign/player/%s" % mpri_Key).data
+    vt = scrapertools.find_single_match(data_vt, 'function\|([^\|]+)\|')
+    if "fallback" in vt:
+        vt = scrapertools.find_single_match(data_vt, 'jwConfig\|([^\|]+)\|')
+
+    media_urls = scrapertools.find_multiple_matches(data, '\{"file"\s*\:\s*"([^"]+)"\s*,\s*"label"\s*\:\s*"([^"]+)"')
+    video_urls = []
+
+    for media_url, label in media_urls:
+        media_url += "?direct=false&ua=1&vt=%s" % vt
         video_urls.append(
-            [scrapertools.get_filename_from_url(media_url)[-4:] + " (" + res + ") [thevideo.me]", media_url])
+            [scrapertools.get_filename_from_url(media_url)[-4:] + " (" + label + ") [thevideo.me]", media_url])
+
     return video_urls
 
 
@@ -50,13 +53,13 @@ def find_videos(data):
     encontrados = set()
     devuelve = []
 
-    patronvideos = "(?:thevideo.me|tvad.me|thevid.net|thevideo.ch|thevideo.us)/(?:embed-|)([A-z0-9]+)"
+    patronvideos = 'thevideo.me/(?:embed-|)([A-z0-9]+)'
     logger.info("#" + patronvideos + "#")
     matches = re.compile(patronvideos, re.DOTALL).findall(data)
 
     for match in matches:
         titulo = "[thevideo.me]"
-        url = "https://thevideo.me/embed-" + match + ".html"
+        url = "http://thevideo.me/embed-" + match + ".html"
         if url not in encontrados:
             logger.info("  url=" + url)
             devuelve.append([titulo, url, 'thevideome'])
