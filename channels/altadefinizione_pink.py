b1ec7542456d354ebe5cf238cc811958e7076984
orione7 <streamondemandpureita@gmail.com>
2018-11-14 18:13:51 -0500

    Update altadefinizione_pink.py

--- channels/altadefinizione_pink.py
+++ channels/altadefinizione_pink.py
@@ -26,7 +26,7 @@ __language__ = "IT"
 
 host = "https://altadefinizione.fm/"
 
-headers = [['User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0'],
+headers = [['User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/63.0'],
            ['Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'],
            ['Accept-Encoding', 'gzip, deflate'],
            ['Referer', host],
