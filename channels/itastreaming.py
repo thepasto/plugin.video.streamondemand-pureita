cb4092f9ac6fb8998609130d4cfb3041da554b57
orione7 <streamondemandpureita@gmail.com>
2018-11-22 17:41:18 -0500

    Update and Fix

--- channels/itastreaming.py
+++ channels/itastreaming.py
@@ -17,7 +17,7 @@ from core.item import Item
 from core.tmdb import infoSod
 
 __channel__ = "itastreaming"
-host = "https://itastreaming.stream"
+host = "https://itastreaming.film"
 headers = [
     ['User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0'],
     ['Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'],
